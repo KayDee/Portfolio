ec0315da4bdf3cd2ba3e3d2fb6dcc3e2f07c0aba
KaustubhD <kaustubhd287@gmail.com>
2018-04-12 15:49:13 +0530

    Added 7 themes

--- src/components/Themes/themeslist.js
+++ src/components/Themes/themeslist.js
@@ -1,6 +1,6 @@
 export const themesList = [
   {
-    name: 'Red',
+    name: 'Black',
     background: 'rgba(0,0,0, 1)',
     text: 'rgba(255, 255, 255, 1)',
     highlight: 'rgba(50, 50, 50, 1)',
@@ -10,12 +10,67 @@ export const themesList = [
   },
   {
     name: 'Teal',
-    background: 'rgba(38,166,154, 1)',
+    background: 'rgba(38, 166, 154, 1)',
     text: 'rgba(255, 255, 255, 1)',
     highlight: 'rgba(100, 190, 186, 0.7)',
     translucent: 'rgba(38, 166, 154, 0.6)',
     shadowDark: 'rgba(0,0,0,0.7)',
     shadowLight: 'rgba(0,0,0,0.5)'
-  }
+  },
+  {
+    name: 'Purple',
+    background: 'rgba(244, 235, 225, 1)',
+    text: 'rgba(81,78,174, 1)',
+    highlight: 'rgba(185,184,222, 0.7)',
+    translucent: 'rgba(244, 235, 225, 0.6)',
+    shadowDark: 'rgba(0,0,0,0.7)',
+    shadowLight: 'rgba(0,0,0,0.5)'
+  },
+  {
+    name: 'Red',
+    background: 'rgba(244, 235, 225, 1)',
+    text: 'rgba(210,45,45, 1)',
+    highlight: 'rgba(232,150,150, 0.7)',
+    translucent: 'rgba(244, 235, 225, 0.6)',
+    shadowDark: 'rgba(0,0,0,0.7)',
+    shadowLight: 'rgba(0,0,0,0.5)'
+  },
+  {
+    name: 'Green',
+    background: 'rgba(243,238,213, 1)',
+    text: 'rgba(64,163,121, 1)',
+    highlight: 'rgba(178,218,201, 0.7)',
+    translucent: 'rgba(243,238,213, 0.6)',
+    shadowDark: 'rgba(0,0,0,0.7)',
+    shadowLight: 'rgba(0,0,0,0.5)'
+  },
+  {
+    name: 'Yellow',
+    background: 'rgba(254,255,96, 1)',
+    text: 'rgba(0, 0, 0, 1)',
+    highlight: 'rgba(200,201,12, 0.7)',
+    translucent: 'rgba(254,255,96, 0.6)',
+    shadowDark: 'rgba(0,0,0,0.7)',
+    shadowLight: 'rgba(0,0,0,0.5)'
+  },
+  {
+    name: 'Orange',
+    background: 'rgba(244, 235, 225, 1)',
+    text: 'rgba(230,108,36, 1)',
+    highlight: 'rgba(242,181,145, 0.7)',
+    translucent: 'rgba(244, 235, 225, 0.6)',
+    shadowDark: 'rgba(0,0,0,0.7)',
+    shadowLight: 'rgba(0,0,0,0.5)'
+  },
+  {
+    name: 'RedInverse',
+    background: 'rgba(210,45,45, 1)',
+    text: 'rgba(244, 235, 225, 1)',
+    highlight: 'rgba(232,150,150, 0.7)',
+    translucent: 'rgba(210,45,45, 0.6)',
+    shadowDark: 'rgba(0,0,0,0.7)',
+    shadowLight: 'rgba(0,0,0,0.5)'
+  },
+  
 ]
 // export default themesList
